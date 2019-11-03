5b2696f3c8516a6943da23df0aac4912795a7d4a
sindri69 <sindrii17@ru.is>
2019-11-03 15:52:43 +0000

    moved some stuff around

--- Control3DProgram.py
+++ Control3DProgram.py
@@ -9,14 +9,13 @@ from pygame.locals import *
 import sys
 import time
 
-from PIL import Image #for the skybox
-
 from Shaders import *
 from Matrices import *
 from Car import *
 from obj_3D_loading import *
 from CarPhysics import *
 from CarSimple import *
+from DrawStuff import *
 
 from playsound import playsound
 
@@ -37,7 +36,7 @@ class GraphicsProgram3D:
         self.projection_matrix.set_perspective(pi/2, 4/3, 0.3, 30)
         self.shader.set_projection_matrix(self.projection_matrix.get_matrix())
 
-        #self.loadskybox()
+       
 
         self.skysphere_shader = SkysphereShader()
         self.skysphere_shader.use()
@@ -58,6 +57,7 @@ class GraphicsProgram3D:
 
         self.angle = 0
 
+        #player one controls
         self.w_key_down = False
         self.s_key_down = False
         self.a_key_down = False
@@ -103,59 +103,7 @@ class GraphicsProgram3D:
         # self.car.update(delta_time)
         print(self.carSimple.position.x, self.carSimple.position.y, self.carSimple.position.z )
         print(self.carSimple.carHeading)
-    def drawCar(self):
-        self.model_matrix.push_matrix()
-        self.model_matrix.add_translation(self.carSimple.position.x, self.carSimple.position.y, self.carSimple.position.z)
-        # self.model_matrix.add_translation(1.0, 1.0, 1.0)
-        self.model_matrix.add_rotateY(self.carSimple.carHeading)
-        self.model_matrix.add_scale(2.0, 1.5, 4.0)
-        self.shader.set_model_matrix(self.model_matrix.matrix)
-        self.shader.set_material_diffuse(Color(0.2,0.8,0.4))
-        self.shader.set_material_shininess(2)
-        self.cube.set_vertices(self.shader)
-        self.cube.draw(self.shader)
-        self.model_matrix.pop_matrix()
 
-    def drawTree(self):
-        self.model_matrix.push_matrix()
-        self.model_matrix.add_translation(3.0, 1.0, 3.0)
-        self.model_matrix.add_scale(2.0, 1.5, 4.0)
-        self.shader.set_model_matrix(self.model_matrix.matrix)
-        self.tree.draw(self.shader)
-        self.model_matrix.pop_matrix()
-
-    def drawGrass(self):
-        self.model_matrix.push_matrix()
-        self.model_matrix.add_scale(3.0, 0.4, 3.0)
-        self.shader.set_model_matrix(self.model_matrix.matrix)
-        self.grass.draw(self.shader)
-        self.model_matrix.pop_matrix()
-
-    
-    def drawtest(self):
-        self.model_matrix.push_matrix()
-        self.model_matrix.add_scale(0.5, 0.5, 0.5)
-        self.model_matrix.add_translation(10, 1.0, 1.0)
-        self.shader.set_model_matrix(self.model_matrix.matrix)
-        self.test.draw(self.shader)
-        self.model_matrix.pop_matrix() 
-
-    def drawLambo(self):
-        self.model_matrix.push_matrix()
-        self.model_matrix.add_scale(2.0, 2.0, 2.0)
-        self.model_matrix.add_translation(2, 1.0, 3.0)
-        self.shader.set_model_matrix(self.model_matrix.matrix)
-        self.lambo.draw(self.shader)
-        self.model_matrix.pop_matrix()    
-
-    def drawForrest(self, howmanytrees):
-        for x in range (1, howmanytrees):
-            self.model_matrix.push_matrix()
-            self.model_matrix.add_translation(1.0 * x, 1.0, 1.0 * x)
-            self.model_matrix.add_scale(2.0, 1.5, 4.0)
-            self.shader.set_model_matrix(self.model_matrix.matrix)
-            self.tree.draw(self.shader)
-            self.model_matrix.pop_matrix()
     def carSimpleMove(self, delta_time):
         if self.w_key_down:
             self.carSimple.carSpeed += 10 * delta_time
@@ -196,21 +144,6 @@ class GraphicsProgram3D:
         else:
             self.car.steering = 0
             self.car.steering = max(-self.car.max_steering, min(self.car.steering, self.car.max_steering))
-
-
-    # def playerMove(self, delta_time):
-
-
-
-        # if self.w_key_down:
-        #     self.view_matrix.slide(0, 0, -2 * delta_time)
-        # if self.s_key_down:
-        #     self.view_matrix.slide(0, 0, 1 * delta_time)
-        # if self.a_key_down:
-        #     self.view_matrix.pitch(-pi * delta_time)
-        # if self.d_key_down:
-        #     self.view_matrix.pitch(pi * delta_time)
-
 
     def display(self):
         glEnable(GL_DEPTH_TEST)  ### --- NEED THIS FOR NORMAL 3D BUT MANY EFFECTS BETTER WITH glDisable(GL_DEPTH_TEST) ... try it! --- ###
@@ -261,20 +194,9 @@ class GraphicsProgram3D:
         # self.shader.set_light_specular(0.8, 0.3, 0.4)
         self.shader.set_light_ambiance(0.1, 0.1, 0.1)
 
-        # self.shader.set_light1_position(Point(1.0, 1.0, 1.0))
-        # self.shader.set_light1_diffuse(0.3, 0.6, 0.2)
-        # self.shader.set_light1_specular(0.8, 0.3, 0.4)
-        # self.shader.set_light1_ambiance(0.1, 0.1, 0.1)
-
-        # self.shader.set_light2_direction(Point(0.0, -1.0, 0.0))
-        # self.shader.set_light2_diffuse(0.4, 0.8, 0.8)
-        # self.shader.set_light2_specular(0.6, 0.3, 0.4)
-        # self.shader.set_light2_ambiance(0.1, 0.0, 0.0)
-
         #cube for now, will be a car later
-        self.drawCar()
-        self.drawTree()
-        
+        drawCar(self)
+        drawTree(self)
         #self.drawGrass()
 
 
@@ -333,17 +255,6 @@ class GraphicsProgram3D:
 
         #OUT OF GAME LOOP
         pygame.quit()
-
-
-    # def drawSphere(self):
-    #     self.sphere.set_vertices(self.shader)
-    #     self.model_matrix.push_matrix()
-    #     self.model_matrix.add_translation(self.ballPosX, 1.0, self.ballPosZ)
-    #     self.shader.set_material_diffuse(self.sphereMatR, self.sphereMatG, self.sphereMatB)
-    #     self.shader.set_material_specular(0.4, 0.4, 0.4)
-    #     self.shader.set_model_matrix(self.model_matrix.matrix)
-    #     self.sphere.draw(self.shader)
-    #     self.model_matrix.pop_matrix()
 
     def start(self):
         self.program_loop()
